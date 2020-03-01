ebb4d606e2423656cd9f549fd31eac138aac17e9
Robin <robinyzg@hotmail.com>
2020-03-01 15:30:49 +0800

    :sparkles: add log

--- command/module/sportKing/sportKing.service.ts
+++ command/module/sportKing/sportKing.service.ts
@@ -12,10 +12,11 @@ export const findUserById = async (id: string): Promise<object> => new Promise(r
 
 export const saveRecord = (r: Record): Promise<object> => new Promise(resolve => {
   const query = Bb.ins.Query('record')
-  query.set('time', r.time)
+  query.set('time', r.time) // set方法不支持number类型，需改Npm package内的set方法支持number
   query.set('userId', r.userId)
 
   query.save().then(res => {
+    console.log('record created:', r.time)
     resolve(res)
   }).catch(err => {
     console.error(err)
@@ -27,19 +28,20 @@ export const createUserIfNotExist = (u: User): Promise<object> => new Promise(re
   query.equalTo('wechatId', '==', u.wechatId)
   query.find().then(res => {
     if (Array.isArray(res) && res.length > 0) {
-      resolve()
+        console.log('user existed:', u.username)
+        resolve()
     } else {
       const q = Bb.ins.Query('user')
       q.set('wechatId', u.wechatId)
       q.set('username', u.username)
-      q.set('profile', u.profile)
+      q.set('profile', u.profile) // set方法不支持object类型，需改Npm package内的set方法支持object
       query.save().then(r => {
+        console.log('user created:', u.username)
         resolve(r)
       }).catch(err => {
         console.error(err)
       })
     }
-
   })
 })
 
